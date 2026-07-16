9a29e2dbefa4b9680cbba9448389a89c0fd4b9d1
ben-c-v <benc.vianney@gmail.com>
2026-07-16 14:37:06 -0400

    Fix validation for main menu options

--- VideoGameShop/Program.cs
+++ VideoGameShop/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.IO;
 // testing git
 namespace VideoGameShop
 {
     internal class Game
     {
-        // Properties of the Game class
+        // Properties
         private int itemNumber;
         private string itemName;
         private double price;
@@ -13,11 +13,9 @@ namespace VideoGameShop
         private int quantity;
 
         // Default constructor
-        public Game()
-        {
-        }
+        public Game() { }
 
-        // Constructor assigning all properties
+        // Parameterized constructor assigning all properties
         public Game(int itemNumber, string itemName, double price, double userRating, int quantity)
         {
             this.itemNumber = itemNumber;
@@ -28,36 +26,16 @@ namespace VideoGameShop
         }
 
         // Accessors (getters)
-        public int GetItemNumber()
-        {
-            return this.itemNumber;
-        }
-
-        public string GetItemName()
-        {
-            return this.itemName;
-        }
-
-        public double GetPrice()
-        {
-            return this.price;
-        }
-
-        public double GetUserRating()
-        {
-            return this.userRating;
-        }
-
-        public int GetQuantity()
-        {
-            return this.quantity;
-        }
+        public int GetItemNumber() { return this.itemNumber; }
+        public string GetItemName() { return this.itemName; }
+        public double GetPrice() { return this.price; }
+        public double GetUserRating() { return this.userRating; }
+        public int GetQuantity() { return this.quantity; }
 
         // Mutators (setters)
         public void SetItemNumber(int itemNumber)
         {
-            if (itemNumber >= 1000 && itemNumber <= 9999)
-            {
+            if (itemNumber >= 1000 && itemNumber <= 9999) {
                 this.itemNumber = itemNumber;
             }
             else
@@ -117,97 +95,149 @@ namespace VideoGameShop
         // Override ToString() method
         public override string ToString()
         {
-            return $"Item Number: {this.itemNumber}\n";
+            return $"Name: {this.itemName} | Item Number: {this.itemNumber} | Price: {this.price} | User Rating: {this.userRating} | Quantity: {this.quantity}";
         }
     }
 
+    internal class Validate
+    {
+        // Validates the user's numerical choice against a possible range
+        public static bool AsMenuInput(string input, int menuRange)
+        {
+            int choice;
+
+            // Perform initial validations
+            if (input == "" || input == null) return false;
+            if (!int.TryParse(input, out choice)) return false;
+
+            // Check if the choice does not fall within the range
+            if (choice < 1 || choice > menuRange) return false;
+
+            return true;
+        }
+    }
 
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Game g1 = new Game(2493, "Minecraft", 24.95, 4.60, 5);
-
-            Console.WriteLine(g1.ToString());
-            int userChoice = 0;
-
-            while (userChoice != 5)
-            {
-                // Display of the start menu
-                Console.WriteLine($"Hello! Welcome to our video game store!\n Please select one of the 5 options:");
-                Console.WriteLine("1) Add a new product");
-                Console.WriteLine("2) Search for a product by Item#");
-                Console.WriteLine("3) Search for a product by price");
-                Console.WriteLine("4) Store statistics");
-                Console.WriteLine("5) Exit Application");
-
-                userChoice = MenuInputValidator();
-
-                if (userChoice == 1)
-                {
-                    AddProduct();
-                }
-
-            }
-            Console.WriteLine("Thanks for visiting the store!");
-        }
-
-        public static int MenuInputValidator()
-        {
-            int userChoice;
-
+            Console.WriteLine($"Hello! Welcome to our video game store!");
+            // Program loop
             while (true)
             {
-                Console.WriteLine();
-                Console.WriteLine("Enter your choice: ");
-                string userInput = Console.ReadLine();
+                // Prompt user with the start menu
+                Console.WriteLine("Please select one of the 5 options:");
+                Console.WriteLine("\t1) Add a new product");
+                Console.WriteLine("\t2) Search for a product by item number");
+                Console.WriteLine("\t3) Search for a product by price");
+                Console.WriteLine("\t4) Store statistics");
+                Console.WriteLine("\t5) Exit Application");
 
-                if (int.TryParse(userInput, out userChoice))
+                Console.Write("> ");
+                string input = Console.ReadLine();
+
+                // Jump out of this iteration if the input is invalid
+                if (!Validate.AsMenuInput(input, 5))
                 {
-                    if (userChoice >= 1 && userChoice <= 5)
-                    {
-                        return userChoice;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a number between 1 and 5");
-                    }
+                    Console.WriteLine("Please enter a correct input from 1 through 5.");
+                    continue;
                 }
-                else
+
+                // Branch the program based on user input
+                int choice = Convert.ToInt16(input);
+                switch (choice)
                 {
-                    Console.WriteLine("Invalid input! Please enter a number between 1 and 5.");
+                    case 1:
+                        AddProduct();
+                        break;
+                    case 2:
+                        SearchByItemNumber();
+                        break;
+                    case 3:
+                        SearchByMaxPrice();
+                        break;
+                    case 4:
+                        GetInventoryStatistics();
+                        break;
+                    case 5:
+                    Console.WriteLine("Thanks for visiting our store!");
+                    Console.ReadKey();
+
+                        // Jump out of the main execution to exit the program
+                        return;
                 }
             }
         }
-
         public static void AddProduct()
         {
+            // todo put this somewhere else to read from the video games file
+            //try
+            //{
+            //    StreamReader myFile = new StreamReader("VideoGames.txt");
+            //    // Reads the first line of text
+            //    string line = myFile.ReadLine();
+
+            //    // Continues to read until you reach the end of file
+            //    while (line != null)
+            //    {
+            //        Console.WriteLine(line);
+            //        line = myFile.ReadLine();
+            //    }
+
+            //    // Close the file
+            //    myFile.Close();
+            //    Console.ReadLine();
+            //}
+            //catch(Exception e)
+            //{
+            //    Console.WriteLine("Exception: " + e.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Executing finally block.");
+            //}
+
+            // Get product information from the user
+            // todo add validation
+            Console.WriteLine("Please enter the product details below.");
+            Console.Write("\tProduct name: ");
+            string name = Console.ReadLine();
+            Console.Write("\tProduct ID: ");
+            string id = Console.ReadLine();
+            Console.Write("\tProduct price: ");
+            string price = Console.ReadLine();
+            Console.Write("\tProduct rating: ");
+            string userRating = Console.ReadLine();
+            Console.Write("\tProduct quantity: ");
+            string quantity = Console.ReadLine();
+
             try
             {
-                StreamReader myFile = new StreamReader("VideoGames.txt");
-                // Reads the first line of text
-                string line = myFile.ReadLine();
-
-                // Continues to read until you reach the end of file
-                while (line != null)
-                {
-                    Console.WriteLine(line);
-                    line = myFile.ReadLine();
-                }
-
-                // Close the file
-                myFile.Close();
-                Console.ReadLine();
+                
             }
             catch(Exception e)
             {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
+                Console.WriteLine(e.Message);
             }
             
+        }
+
+        public static void SearchByItemNumber()
+        {
+            Console.WriteLine("SearchByItemNumber()");
+            // todo
+        }
+
+        public static void SearchByMaxPrice()
+        {
+            Console.WriteLine("SearchByMaxPrice()");
+            // todo
+        }
+
+        public static void GetInventoryStatistics()
+        {
+            Console.WriteLine("GetInventoryStatistics()");
+            // todo
         }
     }
 }
